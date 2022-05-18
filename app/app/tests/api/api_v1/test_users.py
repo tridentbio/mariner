423a5750997af2366de3282e28c16504497b946d
Vitor <vitor.albuquerque@shawandpartners.com>
2022-05-18 18:58:56 -0300

    fix user tests

--- app/app/tests/api/api_v1/test_users.py
+++ app/app/tests/api/api_v1/test_users.py
@@ -15,8 +15,8 @@ def test_get_users_superuser_me(
     r = client.get(f"{settings.API_V1_STR}/users/me", headers=superuser_token_headers)
     current_user = r.json()
     assert current_user
-    assert current_user["is_active"] is True
-    assert current_user["is_superuser"]
+    assert current_user["isActive"] is True
+    assert current_user["isSuperuser"]
     assert current_user["email"] == settings.FIRST_SUPERUSER
 
 
@@ -26,8 +26,8 @@ def test_get_users_normal_user_me(
     r = client.get(f"{settings.API_V1_STR}/users/me", headers=normal_user_token_headers)
     current_user = r.json()
     assert current_user
-    assert current_user["is_active"] is True
-    assert current_user["is_superuser"] is False
+    assert current_user["isActive"] is True
+    assert current_user["isSuperuser"] is False
     assert current_user["email"] == settings.EMAIL_TEST_USER
 
 
--- app/app/utils.py
+++ app/app/utils.py
@@ -5,9 +5,10 @@ from typing import Any, Dict, Optional
 
 import emails
 from emails.template import JinjaTemplate
-from jose import jwt
+from jose import jwt, JWTError
 
 from app.core.config import settings
+
 
 
 def send_email(
@@ -104,5 +105,5 @@ def verify_password_reset_token(token: str) -> Optional[str]:
     try:
         decoded_token = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
         return decoded_token["email"]
-    except jwt.JWTError:
+    except JWTError:
         return None
