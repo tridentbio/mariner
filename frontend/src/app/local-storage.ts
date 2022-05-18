e1b98101e944cdd38729c2810858f538f7c1318d
Vitor <vitor.albuquerque@shawandpartners.com>
2022-05-17 21:01:58 -0300

    backend one level up

--- frontend/src/app/local-storage.ts
+++
@@ -1,2 +0,0 @@
-
-export const TOKEN = 'app-token'
--- frontend/src/features/dashboard/Dashboard.tsx
+++
@@ -1,9 +0,0 @@
-const Dashboard = (props : {}) => {
-  return (
-    <div>
-      This is the dashboard
-    </div>
-  )
-}
-
-export default Dashboard
