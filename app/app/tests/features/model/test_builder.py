d78ba7045f131e9eca1e4e29548bcf88519a6a6a
Vitor Lopes Maia Albuquerque <vitor.albuquerque@shawandpartners.com>
2022-06-08 21:03:33 -0400

    update tests

--- app/app/tests/features/model/test_builder.py
+++ app/app/tests/features/model/test_builder.py
@@ -1,20 +1,25 @@
 import yaml
 from sqlalchemy.orm.session import Session
+from torch import nn
+from torch_geometric.data import DataLoader
 
 from app.features.dataset.crud import repo as datasetsrepo
-from app.features.model.builder import build_dataset, build_model_from_yaml
-from app.features.model.schema import ModelConfig, MoleculeFeaturizerConfig
+from app.features.model.builder import CustomGraphModel, build_dataset
+from app.features.model.schema.configs import (
+    ModelConfig,
+    MoleculeFeaturizerConfig,
+)
 
 
-def test_dataset_creation(db: Session):
+def test_dataset(db: Session):
     yaml_model = "app/tests/data/test_model.yaml"
     with open(yaml_model) as f:
         config_dict = yaml.safe_load(f.read())
         config = ModelConfig.parse_obj(config_dict)
         dataset = build_dataset(config, datasetsrepo, db)
         item = dataset[0]
-        print(item)
         assert item is not None
+        # TODO: assert over item data
 
 
 def test_model_schema():
@@ -27,8 +32,17 @@ def test_model_schema():
         assert feat("CCCC") is not None
 
 
-def test_model_trains():
+def test_model_build(db: Session):
     yaml_model = "app/tests/data/test_model.yaml"
     with open(yaml_model) as f:
-        build_model_from_yaml(f.read())
+        model_config = ModelConfig.from_yaml(f.read())
+        ds = build_dataset(model_config, datasetsrepo, db)
+        model = CustomGraphModel(model_config)
+        assert isinstance(model, nn.Module)
+        loader = DataLoader(ds)
+        batch = next(iter(loader))
+        out = model(batch)
+        print(out)
+        assert out is not None
+        # assert out.size() == y.size()
         # train(model, epochs=2, learning_rate=0.1)
