67e5ee03823b565aa05dc19e7250711307802f95
Vitor Lopes Maia Albuquerque <vitor.albuquerque@shawandpartners.com>
2022-06-13 21:51:06 -0400

    improve tests

--- app/app/tests/api/api_v1/test_models.py
+++ app/app/tests/api/api_v1/test_models.py
@@ -1,14 +1,34 @@
+import io
 import mlflow.pyfunc
-from mlflow.tracking import MlflowClient
+
+from fastapi.datastructures import UploadFile
 from sqlalchemy.orm.session import Session
 from starlette.status import HTTP_200_OK
 from starlette.testclient import TestClient
 
+from app.features.user.model import User
+
 from app.core.config import settings
 from app.features.user.crud import repo as user_repo
-from app.features.model.crud import repo as model_repo
+from app.features.user.model import User
+from app.features.model import controller as model_ctl
+from app.features.model.schema.model import ModelCreate
 from app.tests.utils.utils import random_lower_string
 
+def get_test_user(db: Session) -> User:
+    user = user_repo.get_by_email(db, email=settings.EMAIL_TEST_USER)
+    assert user is not None
+    return user
+
+
+
+def mock_model(created_by: User) -> ModelCreate:
+    return ModelCreate(
+        name=random_lower_string(),
+        model_description=random_lower_string(),
+        model_version_description=random_lower_string(),
+        created_by_id=created_by.id
+    )
 
 def test_post_models_success(
     db: Session,
@@ -16,10 +36,11 @@ def test_post_models_success(
     normal_user_token_headers: dict[str, str],
 ):
     model_path = "app/tests/data/model.pt"
-    user = user_repo.get_by_email(db, email=settings.EMAIL_TEST_USER)
-    model_name: str = random_lower_string()
-    model_description = "test description"
-    model_version_description = "test version description"
+    user = get_test_user(db)
+    model = mock_model(user)
+    model_name = model.name
+    model_description = model.model_description
+    model_version_description = model.model_version_description
     assert user is not None
     with open(model_path, "rb") as f:
         res = client.post(
@@ -43,14 +64,16 @@ def test_post_models_success(
         assert model is not None
 
 
+
 def test_get_models_in_registry(
-    client: TestClient, normal_user_token_headers: dict[str, str]
+    client: TestClient, normal_user_token_headers: dict[str, str],
+    db: Session
 ):
     res = client.get(
         f"{settings.API_V1_STR}/models/", headers=normal_user_token_headers
     )
     assert res.status_code == HTTP_200_OK
-    user = user_repo.get_by_email(db, email=settings.EMAIL_TEST_USER)
+    user = get_test_user(db)
     body = res.json()
     models = body["data"]
 
@@ -59,18 +82,26 @@ def test_get_models_in_registry(
         assert model.created_by_id == user.id
 
 
-def test_model_deploy():
-    raise NotImplemented()
-
+def test_post_models_deployment(db: Session, client: TestClient):
+    user = get_test_user(db)
+    model = mock_model(user.id)
+    latest_version = model
+    model_path = "app/tests/data/model.pt"
+    with open(model_path, 'rb') as f:
+        file = UploadFile('model.pt', io.BytesIO(f.read()))
+        model = model_ctl.create_model(db, model, file)
+    latest_version = model.latest_versions[-1]
+    res = client.post(f"{settings.API_V1_STR}/models/{model.name}/{latest_version.version}/")
+    assert res.status_code == HTTP_200_OK
 
 def test_add_version_to_model():
-    raise NotImplemented()
+   pass
 
 
 def test_update_model():
-    raise NotImplemented()
+    pass
 
 
 def test_delete_model():
-    raise NotImplemented()
+    pass
 
