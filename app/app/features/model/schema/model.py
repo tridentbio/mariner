c9bae68a49280385d2ac6d6d96648139f8939e3e
Vitor Lopes Maia Albuquerque <vitor.albuquerque@shawandpartners.com>
2022-06-16 09:55:53 -0400

    fix(model deployment): install ray with correction version of mlflow. get model_uri from model

--- app/app/features/model/schema/model.py
+++ app/app/features/model/schema/model.py
@@ -13,7 +13,6 @@ class ModelDeployment(ApiBaseModel):
     pass
 
 
-
 class Model(ApiBaseModel):
     name: str
     model_description: Optional[str] = None
@@ -21,6 +20,17 @@ class Model(ApiBaseModel):
     created_by_id: int
     created_by: Optional[User] = None
     latest_versions: List[ModelVersion] = []
+    mlflow_model_data_loaded: bool = False
+
+
+    def get_model_uri(self, version: Optional[str] = None):
+        if not self.mlflow_model_data_loaded:
+            self.load_from_mlflow()
+        if not version:
+            #version = self.latest_versions[-1].version
+            version = '1'
+        return f'model:/{self.name}/{version}'
+
 
     def set_from_mlflow_model(self, mlflow_reg_model: RegisteredModel, versions: List[ModelVersion]):
         """
@@ -30,6 +40,11 @@ class Model(ApiBaseModel):
         self.latest_versions = versions
         if len(versions) > 0:
             self.model_version_description = versions[0].description
+        self.mlflow_model_data_loaded = True
+
+
+    def load_from_mlflow(self):
+        pass
 
 
 class ModelsQuery(PaginatedApiQuery):
