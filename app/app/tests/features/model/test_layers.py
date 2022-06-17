7ca400fa21826b89099d4b21a44dd46c104b8a87
Vitor Lopes Maia Albuquerque <vitor.albuquerque@shawandpartners.com>
2022-06-17 19:54:54 -0400

    fix(tests): fix several tests not updated from last commit

--- app/app/tests/features/model/test_layers.py
+++ app/app/tests/features/model/test_layers.py
@@ -1,11 +1,11 @@
 import torch
 
-from app.features.model.layers.Concat import ConcatLayer
+from app.features.model.layers import Concat
 
 def test_Concat():
     x1 = torch.Tensor([1., 2., 3.])
     x2 = torch.Tensor([1., 2., 3.])
-    concat_layer = ConcatLayer()
+    concat_layer = Concat()
     y = concat_layer(x1, x2)
     assert torch.equal(y, torch.Tensor([1., 2., 3., 1., 2., 3.]))
 
