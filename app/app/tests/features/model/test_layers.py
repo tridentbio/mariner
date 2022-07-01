69feaeeb4cf5c3c58ed1b9c886a2ccd6932e46cf
Vitor Lopes Maia Albuquerque <vitor.albuquerque@shawandpartners.com>
2022-06-30 22:18:32 -0400

    [wip] implement some cases in the forward function
    The graph pooling case is handled, and activation of layers that receive
    a Data object, such as the activations after a message passing or
    pooling layers

--- app/app/tests/features/model/test_layers.py
+++ app/app/tests/features/model/test_layers.py
@@ -9,3 +9,10 @@ def test_Concat():
     concat_layer = Concat()
     y = concat_layer(x1, x2)
     assert torch.equal(y, torch.Tensor([1.0, 2.0, 3.0, 1.0, 2.0, 3.0]))
+
+def test_Concat_other_case():
+    x1 = torch.Tensor([[1.0, 2.0, 3.0]])
+    x2 = torch.Tensor([[1.0, 2.0, 3.0, 4.0]])
+    concat_layer = Concat()
+    y = concat_layer(x1, x2)
+    assert torch.equal(y, torch.Tensor([1.0, 2.0, 3.0, 1.0, 2.0, 3.0, 4.0]))
