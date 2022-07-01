91e18262559dcebf6b71359f425f525eb3ec755a
Vitor Maia <vitor.albuquerque@shawandpartners.com>
2022-07-01 17:29:52 -0300

    feat(model-building): update notebook with pyg dataset creation steps

--- app/app/db/base.py
+++ app/app/db/base.py
@@ -1,8 +1,8 @@
 # Import all the models, so that Base has them before being
 # imported by Alembic
 from app.db.base_class import Base  # noqa
+from app.features.user.model import User  # noqa
 from app.features.dataset.model import ColumnDescription  # noqa
 from app.features.dataset.model import ColumnsMetadata  # noqa
 from app.features.dataset.model import Dataset  # noqa
 from app.features.model.model import Model  # noqa
-from app.features.user.model import User  # noqa
