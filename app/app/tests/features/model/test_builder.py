e3a0a7f63bb717b8862d7a7b876d85e959ff7706
Vitor Maia <vitor.albuquerque@shawandpartners.com>
2022-07-04 13:51:04 -0300

    test: adapt tests to dataset name uniqueness

--- app/app/tests/features/model/test_builder.py
+++ app/app/tests/features/model/test_builder.py
@@ -1,16 +1,16 @@
-import pytest
 import yaml
+from pytorch_lightning import Trainer
 from sqlalchemy.orm.session import Session
 from torch import nn
 from torch_geometric.data import DataLoader
-from pytorch_lightning import Trainer
 
 from app.features.dataset.crud import repo as datasetsrepo
+from app.features.dataset.model import Dataset
 from app.features.model.builder import CustomModel, build_dataset
 from app.features.model.schema.configs import ModelConfig
 
 
-def test_dataset(db: Session):
+def test_dataset(db: Session, some_dataset: Dataset):
     yaml_model = "app/tests/data/test_model_hard.yaml"
     with open(yaml_model) as f:
         config_dict = yaml.safe_load(f.read())
@@ -21,14 +21,14 @@ def test_dataset(db: Session):
         # TODO: assert over item data
 
 
-def test_model_schema():
+def test_model_schema(some_dataset: Dataset):
     yaml_model = "app/tests/data/test_model_hard.yaml"
     with open(yaml_model) as f:
         model = ModelConfig.from_yaml(f.read())
         assert isinstance(model, ModelConfig)
 
 
-def test_model_build(db: Session):
+def test_model_build(db: Session, some_dataset: Dataset):
     yaml_model = "app/tests/data/test_model_hard.yaml"
     with open(yaml_model) as f:
         model_config = ModelConfig.from_yaml(f.read())
