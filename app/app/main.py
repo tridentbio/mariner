f22509ee99c2230c24547538450194098f520db7
Vitor Maia <vitor.albuquerque@shawandpartners.com>
2022-07-28 15:01:03 -0300

    [wip] ws

--- app/app/main.py
+++ app/app/main.py
@@ -1,11 +1,8 @@
-from time import sleep
-
-from fastapi import Depends, WebSocket, WebSocketDisconnect
 from fastapi.applications import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
-from app.api import deps
 from app.api.api_v1.api import api_router
+from app.api.websocket import ws_router
 from app.core.config import settings
 
 app = FastAPI(
@@ -28,20 +25,5 @@ def healthcheck():
     return ""
 
 
-@app.websocket("/ws")
-async def websocket_endpoint(
-    websocket: WebSocket, cookie_or_token: str = Depends(deps.get_cookie_or_token)
-):
-    try:
-        await websocket.accept()
-        await websocket.send_text(
-            f"Session cookie or query token value is: {cookie_or_token}"
-        )
-        while True:
-            await websocket.send_text("Query parameter q is")
-            sleep(5)
-    except WebSocketDisconnect:
-        pass
-
-
 app.include_router(api_router, prefix=settings.API_V1_STR)
+app.include_router(ws_router)
