22101e6029293b761f5fd375d1b53dd1c08db9ed
Vitor Lopes Maia Albuquerque <vitor.albuquerque@shawandpartners.com>
2022-07-29 11:38:06 -0400

    feat: add endpoint to get histories of running training experiments

--- app/app/api/api_v1/endpoints/experiments.py
+++ app/app/api/api_v1/endpoints/experiments.py
@@ -9,6 +9,7 @@ from app.features.experiments import controller as experiments_ctl
 from app.features.experiments.schema import (
     Experiment,
     ListExperimentsQuery,
+    RunningHistory,
     TrainingRequest,
 )
 from app.features.user.model import User
@@ -21,7 +22,7 @@ async def post_experiments(
     request: TrainingRequest,
     db: Session = Depends(deps.get_db),
     current_user: User = Depends(deps.get_current_active_user),
-):
+) -> Experiment:
     result = await experiments_ctl.create_model_traning(db, current_user, request)
     return result
 
@@ -31,6 +32,14 @@ def get_experiments(
     experiments_query: ListExperimentsQuery = Depends(),
     db: Session = Depends(deps.get_db),
     current_user: User = Depends(deps.get_current_active_user),
-):
+) -> List[Experiment]:
     result = experiments_ctl.get_experiments(db, current_user, experiments_query)
     return result
+
+@router.get("/running-history", response_model=List[RunningHistory])
+def get_experiments_running_history(
+    user: User = Depends(deps.get_current_active_user),
+) -> List[RunningHistory]:
+    histories = experiments_ctl.get_running_histories(user)
+    return histories
+
