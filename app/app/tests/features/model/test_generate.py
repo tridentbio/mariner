a3312ac0ff73f78ddbc3416376fc60aae2644d9e
Vitor <vitor.albuquerque@shawandpartners.com>
2022-10-13 11:42:42 -0300

    feat(generate.py): improving code to get the forward signature of the classes

--- app/app/tests/features/model/test_generate.py
+++ app/app/tests/features/model/test_generate.py
@@ -12,3 +12,18 @@ def test_generate_bundle():
         check_output(["python"], input=python_code, text=True)
     except CalledProcessError:
         pytest.fail("Failed to exectute generated python bundle`")
+
+
+# TODO:
+# Parameterized test for some layers and featurizer classes
+# where we check the _get_component_signature properly gets
+# and separates the arguments for each of the test cases
+# - Linear
+# - ReLU
+# - GCN
+# - Some aggregation layer from pygnn
+# - Some pooling layer from pygnn
+# - MoleculeFeaturizer
+# - Concat
+def test_get_component_signature():
+    ...
