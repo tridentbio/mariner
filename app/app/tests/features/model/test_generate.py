2183c82453669b3d76a97f62e5324598be5217d0
Vitor <vitor.albuquerque@shawandpartners.com>
2022-10-13 18:01:53 -0300

    feat: use std python Signature and Parameter and make generated code runnable

--- app/app/tests/features/model/test_generate.py
+++ app/app/tests/features/model/test_generate.py
@@ -6,24 +6,9 @@ from app.features.model import generate
 
 
 def test_generate_bundle():
-    python_code = generate.generate_bundle()
+    python_code = generate.generate_bundlev2()
     assert isinstance(python_code, str)
     try:
         check_output(["python"], input=python_code, text=True)
     except CalledProcessError:
         pytest.fail("Failed to exectute generated python bundle`")
-
-
-# TODO:
-# Parameterized test for some layers and featurizer classes
-# where we check the _get_component_signature properly gets
-# and separates the arguments for each of the test cases
-# - Linear
-# - ReLU
-# - GCN
-# - Some aggregation layer from pygnn
-# - Some pooling layer from pygnn
-# - MoleculeFeaturizer
-# - Concat
-def test_get_component_signature():
-    ...
