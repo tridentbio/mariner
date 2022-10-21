a241ece2ac3f1aae0410d2f59b286bdc5172013b
vilma <vlma@cin.ufpe.br>
2022-10-21 14:44:33 -0300

    chore: delete unused stuff

--- mariner/crud/base.py
+++
@@ -1,7 +0,0 @@
-from typing import Any, Dict, Generic, List, Optional, Type, TypeVar, Union
-
-from fastapi.encoders import jsonable_encoder
-from pydantic import BaseModel
-from sqlalchemy.orm import Session
-
-from app.db.base_class import Base
