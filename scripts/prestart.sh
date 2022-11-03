811f220d680d606c4ed997d4aa20474022148ba8
Vitor Lopes Maia Albuquerque <vitor.albuquerque@shawandpartners.com>
2022-11-03 15:24:38 -0400

    install jupyter as dev dependency

--- scripts/prestart.sh
+++ scripts/prestart.sh
@@ -1,11 +1,4 @@
 #! /usr/bin/env bash
-# Only ran from container
-
-# Let the DB start
-python /app/api/backend_pre_start.py
 
 # Run migrations
 alembic upgrade head
-
-# Create initial data in DB
-python /app/mariner/initial_data.py
