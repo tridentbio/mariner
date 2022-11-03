4c92b5e08a4fa2e50fa07a6b0488f4ec4a5f53c2
Vitor Lopes Maia Albuquerque <vitor.albuquerque@shawandpartners.com>
2022-11-03 15:20:44 -0400

    remove unused functions in api

--- api/api_v1/endpoints/login.py
+++ api/api_v1/endpoints/login.py
@@ -1,21 +1,13 @@
 from typing import Any
 
-from fastapi import APIRouter, Body, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
 from api import deps
 from mariner.exceptions import UserNotActive, UserNotFound
-from mariner.schemas.msg import Msg
 from mariner.schemas.token import Token
-from mariner.core.security import get_password_hash
-from mariner.stores.user_sql import user_store
 from mariner.users import BasicAuth, authenticate
-from mariner.utils import (
-    generate_password_reset_token,
-    send_reset_password_email,
-    verify_password_reset_token,
-)
 
 router = APIRouter()
 
@@ -38,49 +30,3 @@ def login_access_token(
         raise HTTPException(status_code=400, detail="Incorrect email or password")
     except UserNotActive:
         raise HTTPException(status_code=400, detail="Inactive user")
-
-
-@router.post("/password-recovery/{email}", response_model=Msg)
-def recover_password(email: str, db: Session = Depends(deps.get_db)) -> Any:
-    """
-    Password Recovery
-    """
-    user = user_store.get_by_email(db, email=email)
-
-    if not user:
-        raise HTTPException(
-            status_code=404,
-            detail="The user with this username does not exist in the system.",
-        )
-    password_reset_token = generate_password_reset_token(email=email)
-    send_reset_password_email(
-        email_to=user.email, email=email, token=password_reset_token
-    )
-    return {"msg": "Password recovery email sent"}
-
-
-@router.post("/reset-password/", response_model=Msg)
-def reset_password(
-    token: str = Body(...),
-    new_password: str = Body(...),
-    db: Session = Depends(deps.get_db),
-) -> Any:
-    """
-    Reset password
-    """
-    email = verify_password_reset_token(token)
-    if not email:
-        raise HTTPException(status_code=400, detail="Invalid token")
-    user = user_store.get_by_email(db, email=email)
-    if not user:
-        raise HTTPException(
-            status_code=404,
-            detail="The user with this username does not exist in the system.",
-        )
-    elif not user_store.is_active(user):
-        raise HTTPException(status_code=400, detail="Inactive user")
-    hashed_password = get_password_hash(new_password)
-    user.hashed_password = hashed_password
-    db.add(user)
-    db.commit()
-    return {"msg": "Password updated successfully"}
