3ca27638f1aa5ef272f70caad622ce009f31e8f6
Vitor Lopes Maia Albuquerque <vitor.albuquerque@shawandpartners.com>
2022-11-14 10:25:24 -0500

    fix: hard coded redirect url link

--- mariner/oauth.py
+++ mariner/oauth.py
@@ -6,9 +6,7 @@ from mariner.stores.oauth_state_sql import oauth_state_store
 
 def get_github_oauth_url(db: Session):
     state = oauth_state_store.create_state(db, provider="github").state
-
-    # TODO: update to mariner URL
-    redirect_uri = f"http://127.0.0.1:80/api/v1/oauth-callback"
+    redirect_uri = f"{settings.SERVER_HOST}/api/v1/oauth-callback"
     return f"https://github.com/login/oauth/authorize?client_id={settings.GITHUB_CLIENT_ID}&redirect_uri={redirect_uri}&scope=read:user,user:email&state={state}"
 
 
