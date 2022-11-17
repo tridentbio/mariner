41a89a063ea9102d73d27f4619c8d7dbeb2f2de9
Vitor Lopes Maia Albuquerque <vitor.albuquerque@shawandpartners.com>
2022-11-17 09:53:38 -0500

    tests: dataset creation with scaffold split unit test

--- tests/mariner/test_datasets.py
+++ tests/mariner/test_datasets.py
@@ -1,3 +1,6 @@
+from typing import Literal, Optional
+
+import pytest
 from fastapi import UploadFile
 from sqlalchemy.orm import Session
 
@@ -15,38 +18,54 @@ from tests.conftest import get_test_user
 from tests.utils.utils import random_lower_string
 
 
-def test_create_dataset(db: Session):
-    with open("tests/data/Lipophilicity.csv", "rb") as f:
-        colsdescription = [
-            ColumnsDescription(
-                pattern="CMPD_CHEM.*",
-                data_type=StringDataType(domain_kind="string"),
-                description="exp id",
-            ),
-            ColumnsDescription(
-                pattern="exp",
-                data_type=QuantityDataType(unit="mole", domain_kind="numeric"),
-                description="exp id",
-            ),
-            ColumnsDescription(
-                pattern="smiles",
-                data_type=SmileDataType(domain_kind="smiles"),
-                description="exp id",
-            ),
-        ]
-        name = random_lower_string()
-        create_obj = DatasetCreate(
-            file=UploadFile("file", f),
-            name=name,
-            columns_metadata=colsdescription,
-            description=random_lower_string(),
-            split_target=Split("80-10-10"),
-            split_type="random",
-        )
-        user = get_test_user(db)
-        dataset = dataset_ctl.create_dataset(db, user, create_obj)
-        assert dataset
-        assert dataset.name == name
-        db_obj = db.query(Dataset).filter(Dataset.name == name).first()
-        assert db_obj
-        assert db_obj.id
+def mock_dataset_create_request(
+    split_type: Literal["scaffold", "random"] = "random",
+    split_column: Optional[str] = None,
+):
+    f = open("tests/data/Lipophilicity.csv", "rb")
+    colsdescription = [
+        ColumnsDescription(
+            pattern="CMPD_CHEM.*",
+            data_type=StringDataType(domain_kind="string"),
+            description="exp id",
+        ),
+        ColumnsDescription(
+            pattern="exp",
+            data_type=QuantityDataType(unit="mole", domain_kind="numeric"),
+            description="exp id",
+        ),
+        ColumnsDescription(
+            pattern="smiles",
+            data_type=SmileDataType(domain_kind="smiles"),
+            description="exp id",
+        ),
+    ]
+    name = random_lower_string()
+    create_obj = DatasetCreate(
+        file=UploadFile("file", f),
+        name=name,
+        columns_metadata=colsdescription,
+        description=random_lower_string(),
+        split_target=Split("80-10-10"),
+        split_type=split_type,
+        split_column=split_column,
+    )
+    assert create_obj.split_type == split_type
+    return create_obj
+
+
+@pytest.mark.parametrize(
+    "create_obj",
+    [
+        mock_dataset_create_request(split_type="scaffold", split_column="smiles"),
+        mock_dataset_create_request(split_type="random"),
+    ],
+)
+def test_create_dataset(db: Session, create_obj: DatasetCreate):
+    user = get_test_user(db)
+    dataset = dataset_ctl.create_dataset(db, user, create_obj)
+    assert dataset
+    assert dataset.name == create_obj.name
+    db_obj = db.query(Dataset).filter(Dataset.name == create_obj.name).first()
+    assert db_obj
+    assert db_obj.id
