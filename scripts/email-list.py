8c1e91b4923fb4437e66185792a5ee96e18d6f85
anderovsk <anderovsk@pop-os.localdomain>
2022-11-29 21:16:54 -0300

    Fix secrets

--- scripts/email-list.py
+++ scripts/email-list.py
@@ -2,8 +2,8 @@ import sys
  
 emails = str(sys.argv[1])
 print(emails)
-emails_fixed = emails.replace('\n', '')
-print(emails_fixed)
+# emails_fixed = emails.replace('\n', '')
+# print(emails_fixed)
 with open('secrets-example.yaml', 'r') as file :
   filedata = file.read()
 filedata = filedata.replace('#EMAILS', emails_fixed)
