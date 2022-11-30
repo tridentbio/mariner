e41c2cdc0529b20e474e99a650d7e2748034c56b
vilma <vlma@cin.ufpe.br>
2022-11-30 10:56:09 -0300

    feat: checke if smiles are valid on prediction

--- mariner/exceptions/model_exceptions.py
+++ mariner/exceptions/model_exceptions.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class ModelVersionNotFound(Exception):
     pass
 
@@ -8,3 +11,9 @@ class ModelNotFound(Exception):
 
 class ModelNameAlreadyUsed(Exception):
     pass
+
+
+class InvalidDataframe(Exception):
+    def __init__(self, message: str, reasons: List[str]):
+        super().__init__(message)
+        self.reasons = reasons
