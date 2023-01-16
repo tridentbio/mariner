e6d0a24452211c02828b4a333cd2c307d4e2b444
tarcisio-melo-sp <tarcisio.melo@shawandpartners.com>
2023-01-16 20:07:00 -0300

    feat: biological infer data type

--- model_builder/constants.py
+++ model_builder/constants.py
@@ -8,3 +8,38 @@ class TrainingStep(Enum):
     TRAIN = 1
     VAL = 2
     TEST = 3
+
+
+class BiologicalValidChars(Enum):
+    """Used to store list of valid chars
+    for each biological sequences.
+    """
+
+    AMBIGUOUS_DNA_RNA = ["R", "Y", "S", "W", "K", "M", "B", "D", "H", "V", "N"]
+    UNAMBIGUOUS_DNA_RNA = ["A", "C", "G", "T", "U"]
+    ONLY_DNA = "T"
+    ONLY_RNA = "U"
+    PROTEIN = [
+        "A",
+        "C",
+        "D",
+        "E",
+        "F",
+        "G",
+        "H",
+        "I",
+        "K",
+        "L",
+        "M",
+        "N",
+        "P",
+        "Q",
+        "R",
+        "S",
+        "T",
+        "V",
+        "W",
+        "Y",
+        "-",
+        "*",
+    ]
