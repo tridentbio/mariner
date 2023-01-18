5ccdd0bd2ec46e7b67d7296a2725a83df240af37
Vitor Lopes Maia Albuquerque <vitor.albuquerque@shawandpartners.com>
2023-01-18 10:54:35 -0500

    feat: add monitoring callback [WIP]

---
+++ tests/mariner/ray_actors/test_training_actors.py
@@ -0,0 +1,15 @@
+"""Tests the training actor"""
+
+
+class TestTrainingActor:
+    def test_persists_metrics(self):
+        """Checks wheter metrics can be found in expected
+        mlflow location (db) and mariner (db)"""
+        ...
+
+    def test_persists_model(self):
+        """Checks wheter model can be correctly loaded from mlflow
+        registry (by model and model version). Checks if logged models
+        are in the expected s3 artifact path, and mariner model version
+        entity is mapping to the trained mlflow model version"""
+        ...
