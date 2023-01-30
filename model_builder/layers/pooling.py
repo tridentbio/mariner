46759ce267d59efae156cfb745ac0c20c1872552
vilma <vlma@cin.ufpe.br>
2023-01-30 16:15:46 -0300

    fix custom layers

--- model_builder/layers/pooling.py
+++ model_builder/layers/pooling.py
@@ -2,12 +2,27 @@
 Pooling layer defined from torch operations
 """
 
+from typing import Union
+
 from torch import Tensor, nn
 
 
 class AddPooling(nn.Module):
-    def __init__(self):
-        ...
+    """
+    Returns the summation of it's inputs (using tensor.sum(dim=dim))
+
+    See:
+        <https://pytorch.org/docs/stable/generated/torch.sum.html>
+    """
+
+    def __init__(self, dim: Union[int, None] = None):
+        nn.Module.__init__(self)
+        self.dim = dim
 
     def forward(self, x: Tensor):
-        return x.sum()
+        """Returns summation of tensor items
+
+        Args:
+            x: torch.Tensor
+        """
+        return x.sum(dim=self.dim)
