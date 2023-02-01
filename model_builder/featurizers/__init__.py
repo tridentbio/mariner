95f369a62e5b115dfb486a2a93c870707ae80561
Tyler Shimko <tshimko@trident.bio>
2023-02-01 13:59:08 -0800

    Implement full panle of bio seq featurizers

--- model_builder/featurizers/__init__.py
+++ model_builder/featurizers/__init__.py
@@ -2,9 +2,9 @@
 Featurizers for preprocessing inputs
 """
 from .bio_sequence_featurizer import (
-    DNAFeaturizer,
-    ProteinFeaturizer,
-    RNAFeaturizer,
+    DNASequenceFeaturizer,
+    ProteinSequenceFeaturizer,
+    RNASequenceFeaturizer,
     SequenceFeaturizer,
 )
 from .integer_featurizer import IntegerFeaturizer
