104394ca4886adcd03ad9850d9324bf1c36c7762
vilma <vlma@cin.ufpe.br>
2023-02-14 13:10:03 -0300

    update test scripts and add coverage.py for coverage report

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,5 @@
-#!/usr/bin/env bash
-
-set -e
-set -x
-
-pytest --cov=app --cov-report=term-missing app/tests "${@}"
+docker-compose -f docker-compose-cicd.yml up -d --build && \
+docker-compose -f docker-compose-cicd.yml exec backend poetry run coverage run -m pytest 
+docker-compose -f docker-compose-cicd.yml exec backend poetry run coverage json
+docker-compose -f docker-compose-cicd.yml exec backend poetry run coverage html
+docker-compose -f docker-compose-cicd.yml exec backend poetry run coverage report && echo 'Test coverage is good!' || echo 'Test coverage is under 80'
--- scripts/unittest.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-set -x
-pytest --cov=mariner --cov-report=html "${@}" app/tests/features app/tests/core
