4057bb0d95cb965a610530d144c82290ffb445c6
vilma <vlma@cin.ufpe.br>
2023-02-14 13:57:00 -0300

    ci: fix environment used in test

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,4 @@
-docker-compose -f docker-compose-cicd.yml exec backend poetry run coverage run -m pytest 
-docker-compose -f docker-compose-cicd.yml exec backend poetry run coverage json
-docker-compose -f docker-compose-cicd.yml exec backend poetry run coverage html
-docker-compose -f docker-compose-cicd.yml exec backend poetry run coverage report && echo 'Test coverage is good!' || echo 'Test coverage is under 80'
+docker-compose  --env-file .env.test -f docker-compose-cicd.yml exec backend poetry run coverage run -m pytest 
+docker-compose --env-file .env.test -f docker-compose-cicd.yml exec backend poetry run coverage json
+docker-compose --env-file .env.test -f docker-compose-cicd.yml exec backend poetry run coverage html
+docker-compose --env-file .env.test -f docker-compose-cicd.yml exec backend poetry run coverage report && echo 'Test coverage is good!' || echo 'Test coverage is under 80'
