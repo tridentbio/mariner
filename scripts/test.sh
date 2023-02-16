72bfbf4578145ff63035233a4ea187250f1b3be8
vilma <vlma@cin.ufpe.br>
2023-02-16 18:56:20 -0300

    ci: fix environment used in test

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,10 +1,10 @@
-docker compose --env-file .env.test -f docker-compose-cicd.yml \
+docker compose -f docker-compose-cicd.yml \
   exec -T backend poetry run coverage run -m pytest
-docker compose --env-file .env.test -f docker-compose-cicd.yml \
+docker compose -f docker-compose-cicd.yml \
   exec -T backend poetry run coverage json
-docker compose --env-file .env.test -f docker-compose-cicd.yml \
+docker compose -f docker-compose-cicd.yml \
   exec -T backend poetry run coverage html
-docker compose --env-file .env.test -f docker-compose-cicd.yml \
+docker compose -f docker-compose-cicd.yml \
   exec -T backend poetry run coverage report && \
   echo 'Test coverage is good!' || \
   (echo 'Test coverage is under limit' && exit 1)
