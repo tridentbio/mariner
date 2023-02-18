a851aa538477b644857d6109cb126fb9c3eee023
vilma <vlma@cin.ufpe.br>
2023-02-18 18:32:09 -0300

    style: stop linting scripts/*.py files

--- api/main.py
+++ api/main.py
@@ -1,9 +1,14 @@
 """
 Entrypoint to run the mariner API
 """
-import uvicorn
 import os
 
-if __name__ == "__main__":
-    uvicorn.run("api.fastapi_app:app", host="0.0.0.0", port=80, reload=bool(os.getenv('RESTART')))
+import uvicorn
 
+if __name__ == "__main__":
+    uvicorn.run(
+        "api.fastapi_app:app",
+        host="0.0.0.0",
+        port=80,
+        reload=bool(os.getenv("RESTART")),
+    )
