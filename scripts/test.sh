2d97483fee47587383656dc1fade40e2ed2222aa
Vitor Lopes Maia Albuquerque <vlma@cin.ufpe.br>
2023-02-24 14:08:50 -0500

    fix test script [no ci]

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,26 +1,22 @@
 #!/usr/bin/env bash
+
 # Script to run tests with coverage.
 #
 # Run test suites
 echo coverage run "$@"
 coverage run "$@"
 TEST_RESULT=$?
-# Populate cov/json
-[ -n $JSON_OUT ] && coverage json -o $JSON_OUT
+
+# Populate cov/json if JSON_OUT is not empty
+[ ! -z $JSON_OUT ] && coverage json -o $JSON_OUT
+
 # Populate cov/html
 coverage html
 coverage report
-COVERAGE_RESULT=$?
 
 if [[ $TEST_RESULT != 0 ]];
 then
-  echo "Some test failed!";
-  exit 1
-fi
-
-if [[ $COVERAGE_RESULT != 0 ]];
-then
-  echo "Test coverage is not met";
+  echo "Some test failed or coverage is not met!";
   exit 1
 fi
 
