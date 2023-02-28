48fd4c7d27e63ba78aceb1f3e828c6354752f89b
Vitor Maia <vitor.albuquerque@shawandpartners.com>
2023-02-28 13:02:45 -0300

    ci: fix ci (no ci]

--- scripts/doc-coverage.sh
+++ scripts/doc-coverage.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/bash
 
-[ -f $BASE_COV_PATH ] && BASE_COV=$(cat $BASE_COV_PATH) || BASE_COV=0
+if [ -n $BASE_COV_PATH ] && [ -f $BASE_COV_PATH ]; then
+  BASE_COV=$(cat $BASE_COV_PATH) 
+else
+  BASE_COV=0
+fi
 COV=$(docstr-coverage mariner/ model_builder/ api/ --skip-init --skip-magic --skip-private -p)
 if (( $(echo "$COV < $BASE_COV" | bc) )); then
+  echo "New docstring coverage $COV < base docstring doverage $BASE_COV"
   exit 1
 else
   echo $COV
