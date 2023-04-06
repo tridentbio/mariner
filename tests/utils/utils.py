5e4271939340c85af73721480de6697f2d3836e9
tarcisio-melo-sp <tarcisio.melo@shawandpartners.com>
2023-04-06 19:29:17 -0300

    feat: metrics route test

--- tests/utils/utils.py
+++ tests/utils/utils.py
@@ -1,5 +1,6 @@
 import random
 import string
+from typing import Iterator
 
 
 def random_lower_string() -> str:
@@ -8,3 +9,8 @@ def random_lower_string() -> str:
 
 def random_email() -> str:
     return f"{random_lower_string()}@{random_lower_string()}.com"
+
+
+def assert_all_is_number(l: Iterator):
+    for v in l:
+        assert isinstance(v, (int, float)), f"Value {v} is not a number"
