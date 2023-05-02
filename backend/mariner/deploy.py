f91dce2551baf30a72868f2f947d60b371484691
tarcisio-melo-sp <tarcisio.melo@shawandpartners.com>
2023-05-02 10:13:32 -0300

    feat: improve code documentation

--- backend/mariner/deploy.py
+++ backend/mariner/deploy.py
@@ -1,5 +1,5 @@
-"""
-Deploy service
+"""deploy.py
+Module containing deploy service functions.
 """
 from typing import List, Tuple
 
@@ -24,7 +24,16 @@ from mariner.stores.deploy_sql import deploy_store
 def get_deploys(
     db: Session, current_user: User, query: DeploymentsQuery
 ) -> Tuple[List[Deploy], int]:
-    """Retrieve all deploys that requester has access to."""
+    """Retrieve all deploys that the requester has access to.
+
+    Args:
+        db (Session): SQLAlchemy session.
+        current_user (User): Current user.
+        query (DeploymentsQuery): Deployments query object.
+
+    Returns:
+        Tuple[List[Deploy], int]: A tuple containing a list of deploys and the total number of deploys.
+    """
     db_data, total = deploy_store.get_many_paginated(db, query, current_user)
     return db_data, total
 
@@ -33,16 +42,16 @@ def create_deploy(db: Session, current_user: User, deploy_input: DeployBase) -> 
     """Create a deploy for a model version.
 
     Args:
-        db (Session): SQLAlchemy session
-        current_user (User): Current user
-        deploy_input (DeployBase): Deploy input
+        db (Session): SQLAlchemy session.
+        current_user (User): Current user.
+        deploy_input (DeployBase): Deploy input object.
 
     Returns:
-        Deploy: Created deploy
+        Deploy: Created deploy.
 
     Raises:
-        ModelVersionNotFound: If model version does not exist
-        NotCreatorOwner: If user is not the creator of the deploy
+        ModelVersionNotFound: If the model version does not exist.
+        NotCreatorOwner: If the user is not the creator of the deploy.
     """
     deploy_store.get_model_version(db, deploy_input.model_version_id, current_user.id)
 
@@ -65,35 +74,48 @@ def update_deploy(
     """Update a deploy.
 
     Args:
-        db (Session): SQLAlchemy session
-        current_user (User): Current user
-        deploy_id (int): Deploy id
-        deploy_input (DeployUpdateRepo): Deploy input
+        db (Session): SQLAlchemy session.
+        current_user (User): Current user.
+        deploy_id (int): Deploy ID.
+        deploy_input (DeployUpdateRepo): Deploy input object.
 
     Returns:
-        Deploy: Updated deploy
+        Deploy: Updated deploy.
 
     Raises:
-        ModelVersionNotFound: If model version does not exist
-        NotCreatorOwner: If user is not the creator of the deploy
+        ModelVersionNotFound: If the model version does not exist.
+        NotCreatorOwner: If the user is not the creator of the deploy.
     """
-    deploy: DeployEntity = deploy_store.get(db, deploy_id)
-    if not deploy:
+    deploy_entity: DeployEntity = deploy_store.get(db, deploy_id)
+    if not deploy_entity:
         raise DeployNotFound()
-    if deploy.created_by_id != current_user.id:
+    if deploy_entity.created_by_id != current_user.id:
         raise NotCreatorOwner()
 
-    if deploy_input.share_strategy == ShareStrategy.PUBLIC and not deploy.share_url:
-        share_url = generate_deploy_signed_url(deploy.id)
+    if deploy_input.share_strategy == ShareStrategy.PUBLIC and not deploy_entity.share_url:
+        share_url = generate_deploy_signed_url(deploy_entity.id)
         deploy_input.share_url = share_url
 
-    if deploy_input.status != deploy.status:
+    if deploy_input.status != deploy_entity.status:
         ...  # TODO: change service status
 
-    return deploy_store.update(db, db_obj=deploy, obj_in=deploy_input)
+    return deploy_store.update(db, db_obj=deploy_entity, obj_in=deploy_input)
 
 
 def delete_deploy(db: Session, current_user: User, deploy_to_delete_id: int) -> Deploy:
+    """Delete a deploy.
+
+    Args:
+        db (Session): SQLAlchemy session.
+        current_user (User): Current user.
+        deploy_to_delete_id (int): Deploy ID to be deleted.
+
+    Returns:
+        Deploy: Deleted deploy (with updated status).
+
+    Raises:
+        NotCreatorOwner: If the user is not the creator of the deploy.
+    """
     deploy = deploy_store.get(db, deploy_to_delete_id)
     if deploy.created_by_id != current_user.id:
         raise NotCreatorOwner()
@@ -106,8 +128,21 @@ def delete_deploy(db: Session, current_user: User, deploy_to_delete_id: int) -> 
 def create_permission(
     db: Session, current_user: User, permission_input: PermissionCreateRepo
 ) -> Deploy:
-    deploy: DeployEntity = deploy_store.get(db, permission_input.deploy_id)
-    if current_user.id != deploy.created_by_id:
+    """Create a permission for a deploy.
+
+    Args:
+        db (Session): SQLAlchemy session.
+        current_user (User): Current user.
+        permission_input (PermissionCreateRepo): Permission input object.
+
+    Returns:
+        Deploy: Deploy with updated permissions.
+
+    Raises:
+        NotCreatorOwner: If the user is not the creator of the deploy.
+    """
+    deploy_entity: DeployEntity = deploy_store.get(db, permission_input.deploy_id)
+    if current_user.id != deploy_entity.created_by_id:
         raise NotCreatorOwner("Unauthorized.")
     deploy_store.create_permission(db, permission_input)
 
@@ -117,8 +152,21 @@ def create_permission(
 def delete_permission(
     db: Session, current_user: User, permission_input: PermissionCreateRepo
 ) -> Deploy:
-    deploy: DeployEntity = deploy_store.get(db, permission_input.deploy_id)
-    if current_user.id != deploy.created_by_id:
+    """Delete a permission for a deploy.
+
+    Args:
+        db (Session): SQLAlchemy session.
+        current_user (User): Current user.
+        permission_input (PermissionCreateRepo): Permission input object.
+
+    Returns:
+        Deploy: Deploy with updated permissions.
+
+    Raises:
+        NotCreatorOwner: If the user is not the creator of the deploy.
+    """
+    deploy_entity: DeployEntity = deploy_store.get(db, permission_input.deploy_id)
+    if current_user.id != deploy_entity.created_by_id:
         raise NotCreatorOwner("Unauthorized.")
 
     deploy_store.delete_permission(db, permission_input)
