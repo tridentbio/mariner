6b866bfd693d4079d7563ecf0856aaf138cb1f39
tarcisio-melo-sp <tarcisio.melo@shawandpartners.com>
2023-05-04 19:22:16 -0300

    feat: undo one hot layer changes

--- backend/model_builder/layers/one_hot.py
+++ backend/model_builder/layers/one_hot.py
@@ -1,8 +1,7 @@
 """
 OneHot custom layer
 """
-from functools import reduce
-from typing import List, Literal, Union
+from typing import Union
 
 import torch
 from torch import nn
@@ -26,26 +25,22 @@ class OneHot(nn.Module, AutoBuilder):
     def __init__(self):
         nn.Module.__init__(self)
 
-    def forward(self, x1: Union[List[str], List[int]]) -> torch.Tensor:
+    def forward(self, x1: Union[list[str], list[int]]) -> torch.Tensor:
         """One hot representation of the input
-
         Args:
             x1: list of tensors
-
         Returns:
            one hot representation
         """
         assert self.classes, "OneHot layer is missing the classes property set"
-        longs = self.serialize(self.classes, x1, "tensor")
+        longs = torch.Tensor([self.classes[x] for x in x1]).long()
         return F.one_hot(longs, num_classes=len(self.classes)).float()
 
     def set_from_model_schema(self, config, deps):
         """Sets classes dict from the model schema
-
         Args:
             config (ModelSchema): model schema that provides classes
             deps (list[str]): Name of the column received
-
         Raises:
             RuntimeError: If some element in deps is not
             found in the model schema
@@ -63,39 +58,3 @@ class OneHot(nn.Module, AutoBuilder):
                 got_item=column_config,
             )
         self.classes = column_config.data_type.classes
-
-    def serialize(
-        self,
-        classes: dict,
-        xs: List[Union[list, str]],
-        returns_type: Literal["tensor", "list"] = "tensor",
-    ):
-        """Serialize a categorical data into a list of numbers
-
-        e.g.:
-            classes: {'a': 0, 'b': 1}
-            xs: ['a', 'b', ['a', 'b']]
-            -> [0, 1, [0, 1]]
-
-        Args:
-            classes (dict): a dict of classes map to numbers
-            xs (list): a list of keys of classes
-            returns_type (str): return type, either 'tensor' or 'list'
-
-        Returns:
-            a list of numbers or a tensor of numbers
-        """
-        data = reduce(
-            lambda acc, cur: (
-                acc + self.serialize(classes, cur, "list")
-                if isinstance(cur, list)
-                else acc + [classes[cur]]
-            ),
-            xs,
-            [],
-        )
-
-        if returns_type == "list":
-            return data
-        elif returns_type == "tensor":
-            return torch.Tensor(data).long()
