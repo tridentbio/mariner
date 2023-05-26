dfdd4c43317dd6c6f6745c018ea2f9416f485f72
vilma <vitor.albuquerque@shawandpartners.com>
2023-05-26 16:15:41 -0300

    [wip] iterating preprocessing steps in dataset_config

--- backend/fleet/utils/data.py
+++ backend/fleet/utils/data.py
@@ -1,9 +1,17 @@
+"""
+Utilities to build dataset.
+"""
 from typing import Dict, Iterable, Literal, Tuple
 
+import networkx as nx
+import numpy as np
+import sklearn.base
+import torch.nn
 from humps import camel
 
 from fleet.dataset_schemas import DatasetConfig
-from fleet.model_builder.layers_schema import FeaturizersArgsType
+from fleet.model_builder.layers_schema import FeaturizersType
+from fleet.utils.graph import make_graph_from_forward_args
 
 
 class TransformerNormalizer:
@@ -29,7 +37,7 @@ class TransformerNormalizer:
 
 def dataset_topo_sort(
     dataset_config: DatasetConfig,
-) -> Tuple[Iterable[FeaturizersArgsType], Iterable[TransformerNormalizer]]:
+) -> Tuple[Iterable[FeaturizersType], Iterable[TransformerNormalizer]]:
     """Get's the preprocessing pipeline steps in their topological order.
 
     Uses the ``forward_args`` from ``dataset_config.featurizers`` and ``dataset_config.transformer``
@@ -41,6 +49,26 @@ def dataset_topo_sort(
     Returns:
         (featurizers, transforms) tuple, the preprocessing step objects.
     """
+    featurizers_dict = [feat.dict() for feat in dataset_config.featurizers]
+    transforms_dict = [transform.dict() for transform in dataset_config.transforms]
+
+    featurizers_by_name = {feat.name: feat for feat in dataset_config.featurizers}
+    transforms_by_name = {
+        transform.name: transform for transform in dataset_config.transforms
+    }
+
+    graph = make_graph_from_forward_args(featurizers_dict + transforms_dict)
+    topo_sort = nx.topological_sort(graph)
+    featurizers, transforms = [], []
+
+    # TODO: check that featurizers come before transforms
+    for item in topo_sort:
+        if item in featurizers_dict:
+            featurizers.append(featurizers_by_name[item])
+        elif item in transforms_dict:
+            transforms.append(transforms_by_name[item])
+
+    return featurizers, transforms
 
 
 def build_columns_numpy(dataset_config: DatasetConfig):
@@ -68,16 +96,23 @@ def build_columns_numpy(dataset_config: DatasetConfig):
             feat_or_transform.fit_transform(value)
         raise RuntimeError()
 
+    def get_args(data: dict, feat: FeaturizersType) -> np.ndarray:
+        """
+        Get the parameters necessary to pass to feat by processing it's forward_args and looking
+        previously computed values in ``data``
+        """
+        ...
+
     for feat in feats:
-        key, value = get_args(data, feat)
-        apply(feat, value)
-        del data[key]  # a column gets only through one pipeline, therefore we can
+        value = get_args(data, feat)
+        np_array = apply(feat, value)
+        data[feat.name] = np_array
         # erase the last step
 
     for transform in transforms:
         key, value = get_args(data, feat)
-        apply(transform, value)
-        del data[key]
+        np_array = apply(transform, value)
+        data[transform.name] = np_array
 
     return data
 
