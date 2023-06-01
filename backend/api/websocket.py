d9b952787f84b0045c219a1b8be7b79690acc6b6
tarcisio-melo-sp <tarcisio.melo@shawandpartners.com>
2023-06-01 10:26:49 -0300

    feat: multi session

--- backend/api/websocket.py
+++ backend/api/websocket.py
@@ -2,7 +2,7 @@
 Package defines all websocket related functionality.
 """
 import logging
-from typing import Any, Dict, Literal
+from typing import Any, Dict, Literal, List
 
 from fastapi import WebSocket, WebSocketDisconnect
 from fastapi.param_functions import Depends
@@ -15,7 +15,6 @@ from mariner.schemas.api import ApiBaseModel
 
 LOG = logging.getLogger(__name__)
 
-
 class WebSocketMessage(ApiBaseModel):
     """
     Base class for messages exchanged with client
@@ -27,50 +26,80 @@ class WebSocketMessage(ApiBaseModel):
     data: Any
 
 
-def _is_closed(ws: WebSocket) -> bool:
-    """Checks if the websocket is closed.
+class UserConnection:
+    """Represents a user connection to the server
 
-    Args:
-        ws (WebSocket): websocket to check.
-
-    Returns:
-        True if the websocket is closed, False otherwise.
+    Attributes:
+        sessions: list of user sessions connected to the server.
     """
-    return ws.application_state == WebSocketState.DISCONNECTED
+    sessions: List[WebSocket]
+    def __init__(self, session: WebSocket = None):
+        self.sessions = [session] if session else []
+
+    @property
+    def is_active(self) -> bool:
+        """Returns True if there are active connections"""
+        return any([
+            session.application_state == WebSocketState.CONNECTED
+            for session in self.sessions
+        ])
+
+    async def send_message(self, message: WebSocketMessage):
+        """Sends a message to all active connections"""
+        for session in self.sessions:
+            await session.send_text(message.json(by_alias=True))
+    
+    def add_session(self, session: WebSocket):
+        """Adds a session to the active connections"""
+        self.sessions.append(session)
+        return len(self.sessions) - 1
+
+    def remove_session(self, session_idx: int):
+        """Removes a session from the active connections"""
+        self.sessions.pop(session_idx)
 
 
 class ConnectionManager:
+    """Manages all active connections to the server
+    
+    Attributes:
+        active_connections (Dict[int, :class:`UserConnection`])): 
+            dictionary mapping user ids to their websockets.
+    """
     def __init__(self):
-        self.active_connections: Dict[int, WebSocket] = {}
+        self.active_connections: Dict[int, UserConnection] = {}
 
     async def connect(self, user_id: int, websocket: WebSocket):
         """Accepts the connection and track the user id associated with it
-
 
         Args:
             user_id: id from user on connected
             websocket: instance of the server's socket keeping
             the connection
+            
+        Returns:
+            int: index of the connection in the list of connections
         """
-        if user_id in self.active_connections:
-            existing_ws = self.active_connections[user_id]
-            if existing_ws and not _is_closed(existing_ws):
-                try:
-                    await existing_ws.close()
-                except Exception:
-                    # Error raised before: "Trying to send a websocket.close'
-                    # after sending a 'websocket.close'"
-                    LOG.error("Error while closing socket.")
         await websocket.accept()
-        self.active_connections[user_id] = websocket
+        if user_id not in self.active_connections:
+            self.active_connections[user_id] = UserConnection(websocket)
+            return 0
+        else:
+            return self.active_connections[user_id].add_session(websocket)
+        
 
-    def disconnect_session(self, user_id: int):
+    def disconnect_session(self, user_id: int, session_idx: int):
         """Removes the connection from an user
 
         Args:
             user_id: id from user to have websocket disconnected
         """
-        self.active_connections.pop(user_id)
+        if user_id in self.active_connections:
+            self.active_connections[user_id].remove_session(session_idx)
+
+            if not self.active_connections[user_id].is_active:
+                del self.active_connections[user_id]
+
 
     async def send_message(self, user_id: int, message: WebSocketMessage):
         """Sends a message to a specific user
@@ -83,7 +112,8 @@ class ConnectionManager:
         """
         if user_id not in self.active_connections:
             return
-        await self.active_connections[user_id].send_text(message.json(by_alias=True))
+
+        await self.active_connections[user_id].send_message(message)
 
     async def broadcast(self, message: WebSocketMessage):
         """Sends message to all active connections.
@@ -92,7 +122,7 @@ class ConnectionManager:
             message: message to send.
         """
         for connection in self.active_connections.values():
-            await connection.send_text(message.json(by_alias=True))
+            await connection.send_message(message)
 
 
 _manager = None
@@ -126,9 +156,10 @@ async def websocket_endpoint(
             Defaults to Depends(deps.get_current_websocket_user).
     """
     manager = get_websockets_manager()
-    await manager.connect(user.id, websocket)
+    session_idx = await manager.connect(user.id, websocket)
     while websocket.application_state == WebSocketState.CONNECTED:
         try:
             await websocket.receive_text()  # continuously wait for a message
         except (WebSocketDisconnect, AssertionError):
             break
+    manager.disconnect_session(user.id, session_idx)
