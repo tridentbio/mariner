38eeb96e1f38bca7432c9163576edb9fc07487fe
tarcisio-melo-sp <tarcisio.melo@shawandpartners.com>
2023-07-05 10:55:09 -0300

    feat: lint code

--- backend/fleet/scikit_/model_functions.py
+++ backend/fleet/scikit_/model_functions.py
@@ -33,7 +33,9 @@ class SciKitFunctions(BaseModelFunctions):
         model (Union[None, sklearn.base.RegressorMixin, sklearn.base.ClassifierMixin]):
     """
 
-    model: Union[None, sklearn.base.RegressorMixin, sklearn.base.ClassifierMixin] = None
+    model: Union[
+        None, sklearn.base.RegressorMixin, sklearn.base.ClassifierMixin
+    ] = None
     data: Dict[str, Union[pd.Series, np.ndarray, List[np.ndarray]]] = None
 
     def __init__(
@@ -41,7 +43,9 @@ class SciKitFunctions(BaseModelFunctions):
         spec: SklearnModelSpec,
         dataset: pd.DataFrame,
         model: Union[None, sklearn.base.ClassifierMixin] = None,
-        preprocessing_pipeline: Union[None, "data.PreprocessingPipeline"] = None,
+        preprocessing_pipeline: Union[
+            None, "data.PreprocessingPipeline"
+        ] = None,
     ):
         self.spec = spec
         self.dataset = dataset
@@ -76,7 +80,9 @@ class SciKitFunctions(BaseModelFunctions):
             return data_[filtering]
         elif isinstance(data_, list):
             return [
-                data_[i] for i, filter_value in enumerate(filtering) if filter_value
+                data_[i]
+                for i, filter_value in enumerate(filtering)
+                if filter_value
             ]
 
     def _prepare_X_and_y(
@@ -85,7 +91,9 @@ class SciKitFunctions(BaseModelFunctions):
         targets=True,
     ):
         model_config = self.spec.spec
-        assert self.data, "_prepare_data must be called before calling _prepare_X_and_y"
+        assert (
+            self.data
+        ), "_prepare_data must be called before calling _prepare_X_and_y"
         assert self.dataset is not None, "dataset must not be None"
 
         filtering = None
@@ -119,11 +127,17 @@ class SciKitFunctions(BaseModelFunctions):
             prediction = prediction.astype(np.float32)
 
         if step == "train":
-            return self.metrics.get_training_metrics(prediction, batch, sufix=sufix)
+            return self.metrics.get_training_metrics(
+                prediction, batch, sufix=sufix
+            )
         elif step == "val":
-            return self.metrics.get_validation_metrics(prediction, batch, sufix=sufix)
+            return self.metrics.get_validation_metrics(
+                prediction, batch, sufix=sufix
+            )
         elif step == "test":
-            return self.metrics.get_test_metrics(prediction, batch, sufix=sufix)
+            return self.metrics.get_test_metrics(
+                prediction, batch, sufix=sufix
+            )
 
     def train(self):
         """
