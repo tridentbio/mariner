1ccc3a8f40ae048030e8c0f3c76dd2554e19792b
vilma <vitor.albuquerque@shawandpartners.com>
2023-07-21 07:49:22 -0300

    fix: missing merge conflict resolving

--- backend/mariner/core/config.py
+++ backend/mariner/core/config.py
@@ -23,91 +23,6 @@ from pydantic import (
 class Settings(BaseSettings):
     """Models the environment variables used around the application."""
 
-<<<<<<< HEAD
-    API_V1_STR: str = "/api/v1"
-    AUTHENTICATION_SECRET_KEY: str = secrets.token_urlsafe(32)
-    DEPLOYMENT_URL_SIGNATURE_SECRET_KEY: str = secrets.token_urlsafe(32)
-
-    # 60 minutes * 24 hours * 8 days = 8 days
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
-
-    DEPLOYMENT_IDLE_TIME: int = 60 * 10  # 10 minutes
-
-    SERVER_NAME: str
-    SERVER_HOST: AnyHttpUrl
-    WEBAPP_URL: str
-    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
-    ALLOWED_GITHUB_AUTH_EMAILS: List[EmailStr] = []
-
-    @validator("BACKEND_CORS_ORIGINS", pre=True, allow_reuse=True)
-    def assemble_cors_origins(
-        cls, v: Union[str, List[str]]
-    ) -> Union[List[str], str]:
-        """Parses the string of allowed cors origins into a list of strings.
-
-        Returns:
-            list of strings from the array string.
-        """
-        return make_list_from_array_string(v)
-
-    @validator("ALLOWED_GITHUB_AUTH_EMAILS", pre=True, allow_reuse=True)
-    def assemble_allowed_github_auth_emails(
-        cls, v: Union[str, List[str]]
-    ) -> Union[List[str], str]:
-        """Parses the string of allowed cors origins into a list of strings.
-
-        Returns:
-            list of strings from the array string.
-        """
-        return make_list_from_array_string(v)
-
-    PROJECT_NAME: str
-    POSTGRES_SERVER: str
-    POSTGRES_USER: str
-    POSTGRES_PASSWORD: str
-    POSTGRES_DB: str
-    SQLALCHEMY_DATABASE_URI: Optional[PostgresDsn] = None
-
-    @validator("SQLALCHEMY_DATABASE_URI", pre=True, allow_reuse=True)
-    def assemble_db_connection(
-        cls, v: Optional[str], values: Dict[str, Any]
-    ) -> Any:
-        """Assembles the SQLALCHEMY_DATABASE_URI directly from var with same name in .env
-        or from .env POSTGRES_* variables.
-
-        Args:
-            v: SQLALCHEMY_DATABASE_URI if is present on the env file.
-            values: dictionary of unvalidated object.
-
-        Returns:
-            A PostgresDsn object with connection values.
-        """
-        if isinstance(v, str):
-            return v
-        return PostgresDsn.build(
-            scheme="postgresql",
-            user=values.get("POSTGRES_USER"),
-            password=values.get("POSTGRES_PASSWORD"),
-            host=values.get("POSTGRES_SERVER") or "localhost",
-            path=f"/{values.get('POSTGRES_DB') or ''}",
-        )
-
-    EMAILS_ENABLED: bool = False
-    EMAIL_TEST_USER: EmailStr = "test@example.com"  # type: ignore
-
-    AWS_MODE: Literal["sts", "local"] = "local"
-    AWS_ACCESS_KEY_ID: Union[None, str] = None
-    AWS_SECRET_ACCESS_KEY: Union[None, str] = None
-    AWS_REGION: str = "us-east-1"
-    AWS_DATASETS: str = "dev-matiner-datasets"
-    AWS_MODELS: str = "dev-matiner-datasets"
-
-    RAY_ADDRESS: str = "ray://ray-head:10001"
-    APPLICATION_SECRET: str = "replaceme"
-    APPLICATION_CHUNK_SIZE: int = 1024
-
-=======
->>>>>>> origin/develop
     GITHUB_CLIENT_ID: Union[None, str] = None
     GITHUB_CLIENT_SECRET: Union[None, str] = None
 
@@ -199,7 +114,9 @@ class SecretEnv(BaseSettings):
         """
         if values.get("aws_mode") == "local":
             if not values.get("aws_access_key_id"):
-                raise ValueError("aws_access_key_id must be set if aws_mode is local")
+                raise ValueError(
+                    "aws_access_key_id must be set if aws_mode is local"
+                )
             if not values.get("aws_secret_access_key"):
                 raise ValueError(
                     "aws_secret_access_key must be set if aws_mode is local"
@@ -269,7 +186,8 @@ class SettingsV2:
         # load other attributes from conf.toml
         self.server = ServerSettings(
             host=AnyHttpUrl(
-                os.getenv("SERVER_HOST", "http://localhost:8000"), scheme="https"
+                os.getenv("SERVER_HOST", "http://localhost:8000"),
+                scheme="https",
             ),
             cors=[
                 AnyHttpUrl(url, scheme="https")
@@ -282,7 +200,9 @@ class SettingsV2:
         self.test = QA_Test_Settings()
 
         # Get environment variables starting with AUTH
-        auth_env = {k[6:]: v for k, v in os.environ.items() if k.startswith("OAUTH")}
+        auth_env = {
+            k[6:]: v for k, v in os.environ.items() if k.startswith("OAUTH")
+        }
 
         # Get array of providers
         providers = list(set([k.split("_")[0] for k in auth_env.keys()]))
@@ -319,11 +239,13 @@ class SettingsV2:
                     client_id=auth_env[f"{provider}_CLIENT_ID"],
                     client_secret=auth_env[f"{provider}_CLIENT_SECRET"],
                     name=auth_env[f"{provider}_NAME"],
-                    authorization_url=auth_env[f"{provider}_AUTHORIZATION_URL"],
+                    authorization_url=auth_env[
+                        f"{provider}_AUTHORIZATION_URL"
+                    ],
                     allowed_emails=split_if_string(
                         auth_env.get(f"{provider}_ALLOWED_EMAILS", None)
                     ),
-                    scope=auth_env.get(f"{provider}_SCOPE", None),
+                    scope=auth_env.get(f"{provider}_SCOPE", ""),
                     logo_url=auth_env.get(f"{provider}_LOGO_URL", None),
                     jwks_url=auth_env.get(f"{provider}_JWKS_URL", None),
                     token_url=auth_env.get(f"{provider}_TOKEN_URL", None),
@@ -349,17 +271,23 @@ def get_app_settings(
 
 
 @overload
-def get_app_settings(name: Literal["server"], use_cache=False) -> ServerSettings:
+def get_app_settings(
+    name: Literal["server"], use_cache=False
+) -> ServerSettings:
     ...
 
 
 @overload
-def get_app_settings(name: Literal["webapp"], use_cache=False) -> WebappSettings:
+def get_app_settings(
+    name: Literal["webapp"], use_cache=False
+) -> WebappSettings:
     ...
 
 
 @overload
-def get_app_settings(name: Literal["auth"], use_cache=False) -> AuthSettingsDict:
+def get_app_settings(
+    name: Literal["auth"], use_cache=False
+) -> AuthSettingsDict:
     ...
 
 
@@ -369,7 +297,9 @@ def get_app_settings(name: Literal["secrets"], use_cache=False) -> SecretEnv:
 
 
 @overload
-def get_app_settings(name: Literal["services"], use_cache=False) -> ServicesEnv:
+def get_app_settings(
+    name: Literal["services"], use_cache=False
+) -> ServicesEnv:
     ...
 
 
@@ -379,12 +309,16 @@ def get_app_settings(name: Literal["package"], use_cache=False) -> Package:
 
 
 @overload
-def get_app_settings(name: Literal["test"], use_cache=False) -> QA_Test_Settings:
+def get_app_settings(
+    name: Literal["test"], use_cache=False
+) -> QA_Test_Settings:
     ...
 
 
 @overload
-def get_app_settings(name: Literal["tenant"], use_cache=False) -> TenantSettings:
+def get_app_settings(
+    name: Literal["tenant"], use_cache=False
+) -> TenantSettings:
     ...
 
 
