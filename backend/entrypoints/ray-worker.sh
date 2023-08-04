e9a9ead89cbcd33a7ea485587827f8d2016cb2c0
Vitor Maia <vitor.albuquerque@shawandpartners.com>
2023-08-04 14:45:24 -0300

    remove RAY_HEAD_* varialbles

--- backend/entrypoints/ray-worker.sh
+++ backend/entrypoints/ray-worker.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-poetry run ray start --address=$RAY_HEAD_ADDRESS:$RAY_HEAD_PORT --num-cpus=$RAY_NUM_CPU --block 
+poetry run ray start --address=$RAY_ADDRESS:6379 --num-cpus=$RAY_NUM_CPU --block 
