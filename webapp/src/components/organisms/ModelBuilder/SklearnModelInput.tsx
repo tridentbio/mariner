f78e49e0b0fbd37637867e238e4d391de7df6bed
CaioDevSP <caio.cardoso@shawandpartners.com>
2023-08-14 18:57:36 -0300

    test: Implemented E2E tests for the Sklearn model creation form.

--- webapp/src/components/organisms/ModelBuilder/SklearnModelInput.tsx
+++ webapp/src/components/organisms/ModelBuilder/SklearnModelInput.tsx
@@ -55,6 +55,7 @@ export default function SklearnModelInput() {
       render={({ field, fieldState: { error } }) => (
         <PreprocessingStepSelect
           label="Sklearn Model"
+          testId="sklearn-model-select"
           options={options}
           getError={getStepSelectError(
             //@ts-ignore
