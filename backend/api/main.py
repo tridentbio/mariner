e011d78ffd34559da335bafe710917d44efcfd24
Vitor Maia <vitor.albuquerque@shawandpartners.com>
2023-10-03 11:06:41 -0300

    update frontend [no ci]

--- backend/api/main.py
+++ backend/api/main.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     url = urllib.parse.urlparse(get_app_settings("server").host)
     host = "0.0.0.0"
     port = url.port or 80
-    LOG.info("Starting uvicorn app at %s:%d", host, port)
+    LOG.warning("Started uvicorn app at %s:%d", host, port)
     uvicorn.run(
         "api.fastapi_app:app",
         host=host,
